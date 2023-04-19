a3bea74bf1ac4568c4f0b54273afb957669d967f
treym <treymundell03@gmail.com>
2023-04-18 23:14:40 -0500

    layout

--- account.py
+++ account.py
@@ -1,4 +1,4 @@
-class account:
+class Account:
 
     def __init__(self, name):
 
---
+++ test_account.py
@@ -0,0 +1,8 @@
+import pytest
+from account import Account
+
+def test_init():
+
+def test_deposit():
+
+def test_withdraw():
