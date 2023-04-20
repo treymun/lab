2b0425235379ac691af0738e4b54af48dfcd793d
treym <treymundell03@gmail.com>
2023-04-19 19:45:56 -0500

    testing

--- account.py
+++ account.py
@@ -1,11 +1,26 @@
 class Account:
 
     def __init__(self, name):
-
+        self.__account_name = name
+        self.__account_balance = 0
     def deposit(self, amount):
-
+        if amount >= 0:
+            self.__account_balance = self.__account_balance + amount
+            return True
+        else:
+            return False
     def withdraw(self, amount):
+        if amount >= 0:
+            if amount <= self.__account_balance:
+                self.__account_balance = self.__account_balance - amount
+                return True
+            else:
+                return False
+        else:
+            return False
 
     def get_balance(self):
+        return self.__account_balance
 
     def get_name(self):
+        return self.__account_name
